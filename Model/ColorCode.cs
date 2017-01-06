5b8b9878d6addb66aae3be15d6ea06c6d86f2a63
aridai <aridai.net@outlook.jp>
2017-01-06 18:33:33 +0900

    ColorCodeクラスを追加した。

---
+++ Model/ColorCode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Mvvm;
+
+namespace ColorFinder.Model
+{
+    /// <summary>
+    /// カラーコードのRGB値を管理します。
+    /// </summary>
+    public class ColorCode : BindableBase
+    {
+        private byte _R, _G, _B;
+
+        /// <summary>
+        /// R値を取得・設定します。
+        /// </summary>
+        public byte R
+        {
+            get { return _R; }
+            set { SetProperty(ref _R, value); }
+        }
+
+        /// <summary>
+        /// G値を取得・設定します。
+        /// </summary>
+        public byte G
+        {
+            get { return _G; }
+            set { SetProperty(ref _G, value); }
+        }
+        
+        /// <summary>
+        /// B値を取得・設定します。
+        /// </summary>
+        public byte B
+        {
+            get { return _B; }
+            set { SetProperty(ref _B, value); }
+        }
+    }
+}
