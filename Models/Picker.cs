58a11c5c282296872178aaaf4658d21b996162df
aridai <aridai.net@outlook.jp>
2017-01-28 07:44:34 +0900

    Pickerクラスを追加した。

---
+++ Models/Picker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorFinder.Models
+{
+    /// <summary>
+    /// 色の抽出処理を提供します。
+    /// </summary>
+    public class Picker
+    {
+        /// <summary>
+        /// 指定した地点の色を抽出する
+        /// </summary>
+        /// <param name="x">X座標</param>
+        /// <param name="y">Y座標</param>
+        /// <returns></returns>
+        public Color PickUpColor(int x, int y)
+        {
+            var bitmap = new Bitmap(1, 1);
+            var graphics = Graphics.FromImage(bitmap);
+            graphics.CopyFromScreen(new System.Drawing.Point(x, y), new System.Drawing.Point(0, 0), new Size(1, 1));
+            return bitmap.GetPixel(0, 0);
+        }
+    }
+}
