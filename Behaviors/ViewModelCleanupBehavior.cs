77a266a2ad06af17ca69eafc68f3678f6a040fda
aridai <aridai.net@outlook.jp>
2017-02-02 07:47:57 +0900

    ViewModelCleanupBehaviorを追加した。

---
+++ Behaviors/ViewModelCleanupBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace ColorFinder.Behaviors
+{
+    /// <summary>
+    /// ViewModelのリソースの解放を行うビヘイビアを提供します。
+    /// </summary>
+    public class ViewModelCleanupBehavior : Behavior<Window>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Closed += windowClosed;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.Closed -= windowClosed;
+        }
+
+        private void windowClosed(object sender, EventArgs e)
+        {
+            (AssociatedObject.DataContext as IDisposable)?.Dispose();
+        }
+    }
+}
