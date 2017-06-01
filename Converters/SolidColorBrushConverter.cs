c772fc32a37535dc6f625cb89d781d84b426d655
aridai <aridai.net@outlook.jp>
2017-06-01 21:54:56 +0900

    SolidColorBrushConverterを追加した。

---
+++ Converters/SolidColorBrushConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ColorFinder.Converters
+{
+    /// <summary>
+    /// カラーコードをSolidColorBrushに変換するコンバータを提供します。
+    /// </summary>
+    public class SolidColorBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Models.ColorCode color)
+            {
+                return new SolidColorBrush(Color.FromRgb(color.R, color.G, color.B));
+            }
+
+            return new SolidColorBrush();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("SolidColorBrushからColorCodeへの変換はサポートされていません。");
+        }
+    }
+}
